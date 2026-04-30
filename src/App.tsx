40744bb1648bc1f549651cc6f3c2ecb75a4d5e9b
Herfando <herfando_91@yahoo.com>
2026-04-30 16:23:39 +0700

    modified:   package-lock.json         modified:   package.json         deleted:    public/favicon.svg         deleted:    public/icons.svg         new file:   public/models/ToyCar.glb         modified:   src/App.tsx         new file:   src/components/Car.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Box from "./components/Box";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div style={{ height: "100vh" }}>
-      <Canvas>
-        {/* Lighting */}
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[2, 2, 2]} />
-
-        {/* 3D Object */}
-        <Box />
-
-        {/* Controls */}
-        <OrbitControls />
-      </Canvas>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
